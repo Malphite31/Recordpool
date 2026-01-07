c69b6dfaa9ff8f33e08dbd55d661240b10c2e9b4
Malphite31 <jessiebenzs@gmail.com>
2026-01-07 18:40:37 +0800

    Fix Waveform not updating for versions in TrackDetail by adding key prop

--- components/TrackDetail.tsx
+++ components/TrackDetail.tsx
@@ -151,6 +151,7 @@ const TrackDetail: React.FC<TrackDetailProps> = ({
 
             <div className="w-full h-24 md:h-36 mb-8 md:mb-10 relative">
               <WaveformVisualizer
+                key={currentAudioUrl || track.audioUrl}
                 peaks={track.peaks || []}
                 progress={progress}
                 onSeek={handleSeek}
